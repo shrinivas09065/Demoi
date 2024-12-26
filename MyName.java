c6730c155f03483641ee09cdc83d7f7545faaa4e
shrinivas09065 <shrinivas.wable_24uds@sanjivani.edu.in>
2024-12-26 14:21:22 +0530

    Update MyName.java

--- MyName.java
+++ MyName.java
@@ -1,6 +1,6 @@
 import java.*;
 class MyName{
   public static void main(String[] args){
-    System.out.println("My name is Shrinivas");
+    System.out.println("My name is Shrinivas Wable(Patil)");
   }
 }
